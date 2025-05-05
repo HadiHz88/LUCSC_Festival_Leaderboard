d8f8bff611318a16d62ab641e009581c585dead6
Hadi Hz <hijazih24@gmail.com>
2025-05-05 10:09:43 +0300

    Configure TailwindCSS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,23 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title', 'LUCSC Festival')</title>
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @stack('styles')
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>@yield('title', 'LUCSC Festival')</title>
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  @stack('styles')
 </head>
 
 <body class="bg-[#FDFDFC] text-[#1b1b18] min-h-screen">
-    <div class="container mx-auto px-4 py-8">
-        @yield('content')
-    </div>
+  <div class="container mx-auto px-4 py-8">
+    @yield('content')
+  </div>
 
-    @stack('modals')
-    @stack('scripts')
+  @stack('modals')
+  @stack('scripts')
 </body>
 
 </html>
